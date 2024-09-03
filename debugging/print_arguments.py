5a73874389221500e86d4d09c1d78c34221f3f7c
umudovafatima <umudovafatimee@gmail.com>
2024-09-03 16:45:05 +0400

    fixxx

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import sys
-for arg in sys.argv:
-    print(arg)
+
+# Iterate over the arguments starting from index 1 (to skip the script name)
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
